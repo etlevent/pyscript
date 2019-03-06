1fbed9fe8b9bdf1e6c01dc671b0688c411432990
liuhao <liuhao@homelabs.cn>
2019-03-06 18:21:26 +0800

    update code.

--- pyuiautomator.py
+++ pyuiautomator.py
@@ -10,7 +10,7 @@ device = '192.168.1.234'
 def capture(times):
     for i in range(times):
         # 拍照
-        d(resourceId="cn.homelabs.gica:id/capture").click()
+        d(resourceId="%s:id/capture" % package_name).click()
         sleep(6)
         pic_path = r'sdcard/Android/data/%s/files/Pictures/Samples' % package_name
         dst_path = r'E:\111\Samples\%s\Pic-%d' % (device, i)
